1f824cfc0337875524a2d31fd8a676028619f134
Koro0 <ledane92@gmail.com>
2023-01-25 00:30:33 +0100

    add id key

--- src/models/Novel.model.ts
+++ src/models/Novel.model.ts
@@ -1,31 +1,39 @@
-import { Table, Column, Model, DataType, Index } from "sequelize-typescript";
+import { Table, Column, Model, DataType} from "sequelize-typescript";
+import { DataTypes } from "sequelize";
 
 @Table({
   tableName: 'novel'
 })
 export class Novel extends Model {
+  @Column({ 
+    type: DataType.INTEGER.UNSIGNED,
+    autoIncrement: true,
+    primaryKey: true
+  })
+  id?: any;
+  
   @Column({type: DataType.STRING, allowNull:true,})
   image?: string;
 
   @Column({type: DataType.STRING})
-  title: string ='';
+  title?: string;
 
   @Column({type: DataType.STRING})
-  type: string ='';
+  type?: string;
 
   @Column({type: DataType.STRING})
-  author: string='';
+  author?: string;
 
   @Column({type: DataType.STRING})
-  description: string='';
+  description?: string;
 
   @Column({type: DataType.INTEGER})
   like: number=0;
 
-  @Column({ type: DataType.ARRAY(DataType.STRING) })
+  @Column({ type: DataTypes.JSON })
   likesTab?: string[];
   
-  @Column({ type: DataType.ARRAY(DataType.STRING) })
+  @Column({ type: DataTypes.JSON })
   Chapiter?: string[];
 
 }
