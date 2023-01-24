3795252c1892a3ea3efd3b530049bb1c13928a35
Koro0 <ledane92@gmail.com>
2023-01-25 00:30:08 +0100

    add create table in database

--- src/connection/db.config.ts
+++ src/connection/db.config.ts
@@ -1,11 +1,16 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from "sequelize-typescript";
 import dotenv from 'dotenv';
+import { Novel } from "../models/Novel.model";
+import { User } from "../models/User.model";
+import { Product } from "../models/Product.model";
 dotenv.config();
 
 // Créer une connexion à la base de données
-const connection = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!, process.env.DB_PASSWORD, {
+const connection = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!,  process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: "mysql"
 });
+connection.addModels([Novel, User, Product]);
+
 
 export default connection;
