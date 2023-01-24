0f95fcadf85ebc293a34250641836f0927b4bae9
Koro0 <ledane92@gmail.com>
2023-01-24 16:55:02 +0100

    add dev

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,26 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-
 dotenv.config();
+import connection from './connection/db.config';
+import novelRoute from './routes/novel.route';
 
 const app: Express = express();
-const port = 5000;
+const path = require('path');
+const port = process.env.APP_PORT || 5000;
+
+
 
 if (process.env.NODE_ENV === 'development') {
-  app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
-  });
+  connection
+  .sync()
+  .then(()=> {console.log('Database successfully connected')})
+  .catch((err)=>{console.log('Error :', err)});
+
+  app.use('/api/novel', novelRoute);
+  app.use('/images', express.static(path.join(__dirname, 'images')));
+  
 } else {
-  app.get('*', (req: Request, res: Response) => {
+  app.use('*', (req: Request, res: Response) => {
     res.redirect('https://github.com/Koro0/my_books_backend' + req.url);
   });
 }
