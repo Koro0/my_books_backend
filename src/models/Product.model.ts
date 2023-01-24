4fa41a397ee64618d1c65cdc1cd73f3469a59a36
Koro0 <ledane92@gmail.com>
2023-01-25 00:30:50 +0100

    create product models

---
+++ src/models/Product.model.ts
@@ -0,0 +1,23 @@
+import { Table, Column, Model, DataType} from "sequelize-typescript";
+import { DataTypes } from "sequelize";
+
+@Table({
+    tableName: 'product'
+})
+export class Product extends Model {
+    @Column({ 
+        type: DataType.INTEGER.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true
+      })
+      id?: any;
+
+    @Column({type: DataType.INTEGER})
+    prodId?:number;
+
+    @Column({type: DataType.STRING})
+    name?:string;
+
+    @Column({type: DataTypes.JSON})
+    detail?:string[];
+}
