d2390893fec95e9017226d1caf2565505c0c0a10
Koro0 <ledane92@gmail.com>
2023-01-25 01:02:20 +0100

    add cors & express.json

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,38 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response, NextFunction } from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+
 import connection from './connection/db.config';
 import novelRoute from './routes/novel.route';
 
+const cors = require('cors')
 const app: Express = express();
 const path = require('path');
 const port = process.env.APP_PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
 
+app.use((req:Request, res:Response, next:NextFunction) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  );
+  next();
+});
 
 if (process.env.NODE_ENV === 'development') {
   connection
   .sync()
   .then(()=> {console.log('Database successfully connected')})
   .catch((err)=>{console.log('Error :', err)});
+
+  
 
   app.use('/api/novel', novelRoute);
   app.use('/images', express.static(path.join(__dirname, 'images')));
