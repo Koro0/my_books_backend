ec922599842c38e40afb82ad7ece89f61c9c7259
Koro0 <ledane92@gmail.com>
2023-01-25 13:58:16 +0100

    create CRUD operation for novel

--- src/controllers/novel.controller.ts
+++ src/controllers/novel.controller.ts
@@ -1,10 +1,10 @@
 import {Novel} from '../models/Novel.model';
-import {User} from '../models/User.model';
 import { Request, Response } from 'express';
-import fs = require('fs')
+import fs = require('fs');
 
 export const createNovel = async (req:Request, res:Response) => {
     const reqObject = req.body;
+    console.log(req.body);
     const novel = await Novel.create({
         ...reqObject,
         image:`${req.protocol}://${req.get('host')}/images/${
@@ -15,9 +15,34 @@ export const createNovel = async (req:Request, res:Response) => {
         Chapiter: []
     });
     return res
-        .status(200).json({message: "novel createdsuccessfully", data: novel});
+        .status(200).json({message: "novel created successfully", data: novel});
     
 };
-export const hello = async (req:Request, res:Response) => {
-    return res.status(200).json({message:'ok'})
+export const getAllNovel = async (req:Request, res:Response) => {
+    const allNovels : Novel[] = await Novel.findAll();
+    return res.status(200).json({message:'get it is Ok', data: allNovels})
 };
+
+export const getOneNovel = async (req:Request, res:Response) => {
+    const {id} = req.params;
+    console.log(req.params);
+    const novel: Novel | null = await Novel.findByPk(id);
+    return res
+            .status(200).json({message: "Novel fetched successfully", data: novel});
+}
+
+export const updateNovel = async (req:Request, res:Response) => {
+    const {id} = req.params;
+    await Novel.update({...req.body}, {where:{id}});
+    const updateNovel: Novel | null = await Novel.findByPk(id);
+    return res
+            .status(200).json({message:"Novel update successfull", data :updateNovel});
+}
+
+export const deleteNovel = async (req:Request, res:Response) =>{
+    const {id} = req.params;
+    const deleteNovel : Novel | null = await Novel.findByPk(id);
+    await Novel.destroy({where: {id}});
+    return res 
+            .status(200).json({message: "Novel deleted successfull", data: deleteNovel});
+}
