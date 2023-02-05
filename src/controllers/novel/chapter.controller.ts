c4793e9d38a594ea778a7522148689cccded913e
Koro0 <ledane92@gmail.com>
2023-02-05 01:53:11 +0100

    change create chapter

--- src/controllers/novel/chapter.controller.ts
+++ src/controllers/novel/chapter.controller.ts
@@ -1,10 +1,11 @@
 import {Novel} from '../../models/Novel.model';
+import { Chapter } from '../../models/chapter.model';
 import { Request, Response } from 'express';
 
 export const createChapter = async (req:Request, res:Response) => {
-    const {id} = req.params;
+    const {novelId} = req.params;
     const {data} = req.body;
-    await Novel.findByPk(id)
+   /* await Novel.findByPk(id)
         .then((novel: Novel | null) => {
             if(!novel) {
                 return res.status(404).json({message: 'Novel not found'})
@@ -16,6 +17,21 @@ export const createChapter = async (req:Request, res:Response) => {
                 return Novel.update({ chapters: updatedData},{where: {id:id}});
             }
         })
+        .catch((error: Error) => {
+            res.status(500).json({error: error.message});
+        })*/
+    try{
+        const novel = await Novel.findByPk(novelId);
+        if(!novel) {
+            return res.status(404).send({error:'Novel not found'});
+        }
+
+        const chapter = await Chapter.create({...data, novelId});
+        res.send(chapter);
+    } 
+    catch(error) {
+        res.status(500).json({error});
+    }
     
 }
 
