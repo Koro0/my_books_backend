e5f870de322f48488878c1cf8677e442c250ba61
Koro0 <ledane92@gmail.com>
2023-02-11 17:59:17 +0100

    change params & delete old Img with PUT

--- src/controllers/novel.controller.ts
+++ src/controllers/novel.controller.ts
@@ -36,41 +36,48 @@ export const getAllNovel = async (req:Request, res:Response) => {
 };
 
 export const getOneNovel = async (req:Request, res:Response) => {
-    const {id} = req.params;
-    console.log(req.params);
-    const novel: Novel | null = await Novel.findByPk(id);
+    const novelId = req.params.novelId;
+    console.log(req.params.novelId);
+    const novel: Novel | null = await Novel.findByPk(novelId);
     return res
             .status(200).json({message: "Novel fetched successfully", data: novel});
 }
 
 export const updateNovel = async (req:Request, res:Response) => {
-    const {id} = req.params;
+    const novelId = req.params.novelId;
     if(req.file == null){
-        await Novel.update({...req.body},{where:{id}});
+        await Novel.update({...req.body},{where:{novelId}});
     } else {
+        const forDeleteImg : Novel | null = await Novel.findByPk(novelId);
+        const filename:any= forDeleteImg?.image?.split('/')[4];
+        console.log(filename);
+        fs.unlink( `src/images/${filename}`, (err) => {
+            if(err) {res.status(500).json({message: "Could not delete the file. " + err})}
+            else{console.log('deleted image')};
+        });
         await Novel.update({...req.body,
             image:`${req.protocol}://${req.get('host')}/images/${
                 req.file?.filename
-              }`}, {where:{id}});
+              }`}, {where:{novelId}});
     }
-    const updateNovel: Novel | null = await Novel.findByPk(id);
+    const updatedNovel: Novel | null = await Novel.findByPk(novelId);
     return res
-            .status(200).json({message:"Novel update successfull", data :updateNovel});
+            .status(200).json({message:"Novel update successfull", data :updatedNovel});
 }
 
 export const deleteNovel = async (req:Request, res:Response) =>{
-    const {id} = req.params;
-    const deleteNovel : Novel | null = await Novel.findByPk(id);
+    const novelId = req.params.novelId;
+    const deleteNovel : Novel | null = await Novel.findByPk(novelId);
     
     if(deleteNovel?.image){
-        const filename :string = deleteNovel.image.split('/')[4];
+        const filename :string = deleteNovel.image.split('/')[4]; 
         console.log(filename);
         fs.unlink( `src/images/${filename}`, (err) => {
             if(err) {res.status(500).json({message: "Could not delete the file. " + err})}
             else{console.log('deleted image')};
         });
     }
-    await Novel.destroy({where: {id}});
+    await Novel.destroy({where: {novelId}});
     return res 
             .status(200).json({message: "Novel deleted successfull", data: deleteNovel});
 }
