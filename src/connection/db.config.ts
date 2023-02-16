679a7e0310e873d236415a2a67a6eb363fd2fa4a
Koro0 <ledane92@gmail.com>
2023-02-16 14:52:45 +0100

    add likes tab

--- src/connection/db.config.ts
+++ src/connection/db.config.ts
@@ -4,6 +4,7 @@ import { Novel } from "../models/Novel.model";
 import { User } from "../models/User.model";
 import { Product } from "../models/Product.model";
 import { Chapter } from "../models/chapter.model";
+import { LikesTab } from "../models/likesTabs.model";
 dotenv.config();
 
 // Créer une connexion à la base de données
@@ -11,7 +12,7 @@ const connection = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!,  pr
   host: process.env.DB_HOST,
   dialect: "mysql"
 });
-connection.addModels([Novel, User, Product, Chapter]); 
+connection.addModels([Novel, User, Product, Chapter, LikesTab]); 
 
 
 export default connection;
