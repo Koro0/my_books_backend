883f2191f60effbe2214aa461fcc14643b7b2234
Koro0 <ledane92@gmail.com>
2023-02-17 21:07:52 +0100

    change createChapter code & add getAllchapter,  geOneChapter

--- src/controllers/novel/chapter.controller.ts
+++ src/controllers/novel/chapter.controller.ts
@@ -3,49 +3,62 @@ import { Chapter } from '../../models/chapter.model';
 import { Request, Response } from 'express';
 
 export const createChapter = async (req:Request, res:Response) => {
-    const {novelId} = req.params;
-    const {data} = req.body;
-   /* await Novel.findByPk(id)
-        .then((novel: Novel | null) => {
-            if(!novel) {
-                return res.status(404).json({message: 'Novel not found'})
-            }
-            if(!novel.chapters) {
-                return res.status(400).json({message: 'Chapters not found in this Novel'})
-            } else {
-                const updatedData = { ...novel.chapters, ...data,};
-                return Novel.update({ chapters: updatedData},{where: {id:id}});
-            }
-        })
-        .catch((error: Error) => {
-            res.status(500).json({error: error.message});
-        })*/
     try{
-        const novel = await Novel.findByPk(novelId);
-        if(!novel) {
-            return res.status(404).send({error:'Novel not found'});
+        const novelId  = req.params.novelId;
+        const {chapterNumber, title, content} = req.body;
+        if(!chapterNumber || !title || !content) {
+            return res.status(400).json({
+                message: 'toutes les champs doivent être completer'
+            })
         }
-
-        const chapter = await Chapter.create({...data, novelId});
-        res.send(chapter);
+        const newChapter = await Chapter.create({
+            chapterNumber,
+            title,
+            content,
+            novelId
+        });
+        return res
+        .status(201).json({
+            message: 'Chapter created successfully',
+            chapter: newChapter
+        })
     } 
     catch(error) {
         res.status(500).json({error});
     }
-    
 }
 
-export const getChapter = async (req:Request, res:Response) => {
-    const {id} = req.params;
-    await Novel.findByPk(id)
-    .then((novel: Novel | null) => {
-        if(!novel) {
+export const getAllChapters = async (req:Request, res:Response) => {
+    const novel_id = req.params.novelId;
+    await Chapter.findAll({
+        where: {
+            novelId : novel_id
+    }})
+    .then((chapters) => {
+        if(!chapters) {
             return res.status(404).json({message: 'Novel not found'})
         }
-        if(!novel.chapters) {
-            return res.status(400).json({message: 'Chapters not found in this Novel'})
+        return res.status(200).json({data: chapters})
+    })
+    .catch((error: Error) => {
+        res.status(500).json({error: error.message});
+    })
+}
+
+
+export const getOneChapter = async (req:Request, res:Response) => {
+    const novelId = req.params.novelId;
+    const chapterNumber = req.params.chapterId;
+    await Chapter.findOne({
+        where: {
+            novelId,
+            chapterNumber
+    }})
+    .then((chapter) => {
+        if(!chapter) {
+            return res.status(404).json({message: 'chapter not found'})
         }
-        return res.status(200).json({chapters: novel.chapters})
+        return res.status(200).json({chapter: chapter})
     })
     .catch((error: Error) => {
         res.status(500).json({error: error.message});
