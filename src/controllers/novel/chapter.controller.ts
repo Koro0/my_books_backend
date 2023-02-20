0b38f2eddc203507d153ff92c18186c916609f85
Koro0 <ledane92@gmail.com>
2023-02-20 16:59:35 +0100

    add comments

--- src/controllers/novel/chapter.controller.ts
+++ src/controllers/novel/chapter.controller.ts
@@ -1,6 +1,12 @@
 import { Chapter } from '../../models/chapter.model';
 import { Request, Response } from 'express';
 
+/**
+ * 
+ * @param NOVEL_ID get and parse novel Id
+ * @param Chapter.findOrCreate find if chapter is existed else create with defauts
+ * @returns message with status
+ */
 export const createChapter = async (req:Request, res:Response) => {
     try{
         const NOVEL_ID: number  = parseInt(req.params.novelId, 10);
@@ -38,6 +44,12 @@ export const createChapter = async (req:Request, res:Response) => {
     }
 }
 
+/**
+ * 
+ * @param Chapter.findAll find all chapters with novel Id 
+ * @param NOVEL_ID get and parse novel Id
+ * @return all data chapters or error 
+ */
 export const getAllChapters = async (req:Request, res:Response) => {
     const NOVEL_ID = req.params.novelId;
     await Chapter.findAll({
@@ -55,15 +67,20 @@ export const getAllChapters = async (req:Request, res:Response) => {
     })
 }
 
-
+/**
+ * 
+ * @param NOVEL_ID get and parse novel Id 
+ * @param CHAPTER_NUMBER get chapter Number in params
+ * @param Chapter.findOne find Chapter with NOVEL_ID and CHAPTER_NUMBER
+ */
 export const getOneChapter = async (req:Request, res:Response) => {
     const NOVEL_ID = req.params.novelId;
     const CHAPTER_NUMBER = req.params.chapterId;
     await Chapter.findOne({
         where: {
-            NOVEL_ID,
-            CHAPTER_NUMBER
-    }})
+            chapterNumber:CHAPTER_NUMBER,
+            novelId:NOVEL_ID
+        }})
     .then((chapter) => {
         if(!chapter) {
             return res.status(404).json({message: 'chapter not found'})
