ae37a7ec83cacb0cc75592ace759694fc7792f71
Koro0 <ledane92@gmail.com>
2023-02-23 16:37:06 +0100

    add recipe controler

---
+++ src/controllers/Recipe/recipe.controllers.ts
@@ -0,0 +1,26 @@
+import { Recipe } from "../../models/recipe.model/Recipe.model";
+import { Etape } from "../../models/recipe.model/etape.model";
+import { Request, Response } from "express";
+
+export const createRecipe = async (req:Request, res:Response) => {
+    const REQUEST_NEW_RECIPE = req.body;
+    if(req.file) {
+        const recipe = await Recipe.create({
+            ...REQUEST_NEW_RECIPE,
+            image:`${req.protocol}://${req.get('host')}/images/${
+                req.file?.filename
+              }`,
+            likes:[],
+            etape:[]
+        });
+        return res.status(201).json({message: 'New recipe created !', data: recipe});
+    } else {
+        const recipe = await Recipe.create({
+            ...REQUEST_NEW_RECIPE,
+            image: null,
+            likes:[],
+            etape: []
+        });
+        return res.status(201).json({message: 'New recipe created !', data: recipe});
+    }
+}
