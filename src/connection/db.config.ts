e32744e1b6a3ca7ceadaae1c4c8b720ad21b1f8b
Koro0 <ledane92@gmail.com>
2023-03-15 17:32:40 +0100

    change ordre

--- src/connection/db.config.ts
+++ src/connection/db.config.ts
@@ -1,20 +1,27 @@
 import { Sequelize } from "sequelize-typescript";
 import dotenv from 'dotenv';
-import { Novel } from "../models/novel/Novel.model";
+dotenv.config();
+
+//recup les models
 import { User } from "../models/User.model";
+import { Recipe } from "../models/recipe/Recipe.model";
+import { Etape } from "../models/recipe/Etape.model";
+import { Novel } from "../models/novel/Novel.model";
 import { Product } from "../models/scan/Product.model";
 import { Chapter } from "../models/novel/Chapter.model";
 import { LikesTab } from "../models/novel/LikesTabs.model";
-import { Etape } from "../models/recipe.model/etape.model";
-import { Recipe } from "../models/recipe.model/Recipe.model";
-dotenv.config();
 
 // Créer une connexion à la base de données
-const connection = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!,  process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: "mysql"
-});
-connection.addModels([Novel, User, Product, Chapter, LikesTab, Etape, Recipe]); 
+const connection = new Sequelize(
+  process.env.DB_NAME!, 
+  process.env.DB_USER!, 
+  process.env.DB_PASSWORD!, 
+  {
+    host: process.env.DB_HOST!,
+    dialect: "mysql"
+  }
+);
+connection.addModels([User, Recipe, Etape, Novel, Product, Chapter, LikesTab]); 
 
 
-export default connection;
+export default connection 
