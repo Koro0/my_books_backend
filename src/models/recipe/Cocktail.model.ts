0a46044a90a3973c779e65b36b982e3c649ae370
Koro0 <ledane92@gmail.com>
2023-03-20 14:46:50 +0100

    create cocktail model

---
+++ src/models/recipe/Cocktail.model.ts
@@ -0,0 +1,34 @@
+import { Table, Column, Model, DataType, HasMany, BelongsTo, ForeignKey } from "sequelize-typescript";
+import {Ingredients} from './Ingredients.model';
+
+@Table({
+    tableName: 'Cocktail'
+})
+export class Cocktail extends Model {
+    @Column({
+        type: DataType.INTEGER.UNSIGNED,
+        autoIncrement:true,
+        primaryKey:true
+    })
+    cocktailId!:number;
+
+    @Column({type:DataType.STRING})
+    title!: string;
+
+    @Column({type:DataType.STRING})
+    difficulty?:string;
+
+    @Column({type:DataType.STRING})
+    portion?:string;
+
+    @Column({type:DataType.STRING})
+    time?:string;
+    
+    @Column({type:DataType.STRING})
+    description?:string;
+
+    @HasMany(()=> Ingredients)
+    ingredients?:string[];
+
+
+}
