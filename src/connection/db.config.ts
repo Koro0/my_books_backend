849711a8800356e8ad650fccc39f037f6748822b
Koro0 <ledane92@gmail.com>
2023-03-21 19:46:56 +0100

    add new model

--- src/connection/db.config.ts
+++ src/connection/db.config.ts
@@ -8,7 +8,10 @@ import { Recipe } from "../models/recipe/Recipe.model";
 import { Novel } from "../models/novel/Novel.model";
 import { Product } from "../models/scan/Product.model";
 import { Chapter } from "../models/novel/Chapter.model";
-import { LikesTab } from "../models/novel/LikesTabs.model";
+import { LikesTab } from "../models/LikesTabs.model";
+import { Cocktail } from "../models/recipe/Cocktail.model";
+import { Ingredients } from "../models/recipe/Ingredients.model";
+import { Method } from "../models/recipe/Method.model";
 
 // Créer une connexion à la base de données
 const connection = new Sequelize(
@@ -20,7 +23,7 @@ const connection = new Sequelize(
     dialect: "mysql"
   }
 );
-connection.addModels([User, Recipe, Novel, Product, Chapter, LikesTab]); 
+connection.addModels([User, Recipe, Novel, Product, Chapter, LikesTab, Cocktail, Ingredients, Method]); 
 
 
 export default connection 
