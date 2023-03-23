bdd6939b2f79645437e538f0ddc132c4891708dd
Koro0 <ledane92@gmail.com>
2023-03-23 16:56:56 +0100

    add route cocktail

--- src/routes/recipe.route.ts
+++ src/routes/recipe.route.ts
@@ -4,10 +4,13 @@ import {
     getAllRecipe,
 } from '../controllers/Recipe/recipe.controllers';
 
+import { createCocktail } from '../controllers/Recipe/cocktail.controllers';
+
 const multer = require('../middlewares/multer-config');
 const router: Router = Router();
 
 router.post('/', multer, createRecipe);
 router.get('/',  getAllRecipe);
 
+router.post('/cocktail', multer, createCocktail);
 export default router;
