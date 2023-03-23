f7e258585dc99ab620b4a7b77c16f258c3282a70
Koro0 <ledane92@gmail.com>
2023-03-23 16:58:50 +0100

    modify createRecipe Ctrl

--- src/controllers/Recipe/recipe.controllers.ts
+++ src/controllers/Recipe/recipe.controllers.ts
@@ -1,27 +1,38 @@
 import { Recipe } from "../../models/recipe/Recipe.model";
-import { Etape } from "../../models/recipe/etape.model";
 import { Request, Response } from "express";
+import { Ingredient } from "../../models/recipe/Ingredient.model";
+import { Method } from "../../models/recipe/Method.model";
 
 export const createRecipe = async (req:Request, res:Response) => {
-    const REQUEST_NEW_RECIPE = req.body;
-    if(req.file) {
-        const recipe = await Recipe.create({
-            ...REQUEST_NEW_RECIPE,
-            image:`${req.protocol}://${req.get('host')}/images/${
-                req.file?.filename
-              }`,
-            likes:[],
-            etape:[]
-        });
-        return res.status(201).json({message: 'New recipe created !', data: recipe});
-    } else {
-        const recipe = await Recipe.create({
-            ...REQUEST_NEW_RECIPE,
-            image: null,
-            likes:[],
-            etape: []
-        });
-        return res.status(201).json({message: 'New recipe created !', data: recipe});
+    try {
+        const {title, content, ingredientList, methodList} = req.body;
+        
+            const recipe = await Recipe.create({
+                title, 
+                content,
+                image: (req.file ?`${req.protocol}://${req.get('host')}/images/${
+                    req.file?.filename
+                }`: null),
+                likes:[]
+            });
+        
+        //ajout ingredients
+        for (const ingredientData of ingredientList) {
+            const ingredient = await Ingredient.create(ingredientData);
+            await recipe.addIngredient(ingredient);
+        }
+       //ajout ingredients
+       for (const methodData of methodList) {
+        const method = await Method.create(methodData);
+        await recipe.addMethod(method);
+        }
+
+        return res.status(201).json({message: 'New recipe created !', recipe});
+
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({message: "Erreur: create recette de cuisine !"})
     }
 }
 
