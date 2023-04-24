050fe7b35a21d10d56356852b2e8cfc61897d607
Koro0 <ledane92@gmail.com>
2023-04-24 16:08:13 +0200

    add delete comment function

--- src/controllers/comments.controllers.ts
+++ src/controllers/comments.controllers.ts
@@ -1,104 +1,153 @@
 import { Request, Response } from "express";
 import { Comments } from "../models/Comments.models";
-import { Recipe } from "../models/recipe/Recipe.model";
+import { User } from "../models/User.model";
 import { Novel } from "../models/novel/Novel.model";
 import { Cocktail } from "../models/recipe/Cocktail.model";
 
-export const createCommentRecipe =async (res:Response, req:Request) => {
-    const recipeId = req.params.recipeId;
-    const authId = req.auth;
+
+/***
+ * create & get && delete recipe Comments
+ */
+export const createCommentRecipe =async (req:Request, res:Response) => {
     try {
-        const { userId, commentText } = req.body;
-        if (!userId || !commentText) {
+        const { commentText , recipeId, userId } = req.body;
+        if (!commentText || !recipeId || !userId) {
             return res.status(400).json({msg: "Contenu obligatoire"})
         }
+        const isAdmin = await User.findOne({where:{userId:userId, adminStatus: 0}})
+        if(!isAdmin) {
+            return res.status(400).json({msg:"Droit adminitrateur requis"})
+        }
         const NEW_COMMENT = await Comments.create({
-            userId:authId,
+            userId,
             commentText,
             recipeId,
         }) 
-        return res.status(201).json({msg:"commentaire créé", NEW_COMMENT})
+        res.status(201).json({msg:"commentaire créé", NEW_COMMENT})
     }
-    catch (err) {
-        return res.status(400).json({msg:"Erreur creation de commentaire", err})
+    catch (err) { 
+        res.status(400).json({msg:"Erreur creation de commentaire", err})
     }
 }
 
-export const getAllRecipeComment =async (res:Response, req:Request) => {
-    const recipeId = req.params.recipeId;
+export const getAllRecipeComment =async (req:Request, res:Response) => {
+    const ID = req.params.id;
     try {
-        const comments = await Comments.findAll({where:{recipeId:recipeId}})
+        const comments = await Comments.findAll({where:{recipeId:ID}})
         if(!comments) {
             return res.status(400).json({msg:"Aucun commentaires retrouvées"})
         }
-        return res.status(200).json({comments})
+        res.status(200).json({comments})
     } catch (error) {
-        return res.status(400).json({msg: "Erreur de recuperation des commentaires des recettes", error})
+        res.status(400).json({msg: "Erreur de recuperation des commentaires des recettes", error})
     }
 }
 
-export const createCommentNovel =async (res:Response, req:Request) => {
-    const novelId = req.params.novelId;
-    const authId = req.auth;
+export const deleteCommentRecipe =async (req:Request, res:Response) => {
+    const ID = req.params.id;
     try {
-        const { userId, commentText } = req.body;
-        if (!userId || !commentText) {
+        const commentToDelected = await Comments.destroy({where:{commentId:ID}});
+        if(!commentToDelected) {
+            return res.status(400).json({msg: "Commentaire introuvable"})
+        }
+        return res.status(200).json({msg: "Commentaire supprimé avec succes"})
+    } catch (error) {
+        return res.status(400).json({msg: "Erreur suppression du commentaire"})
+    }
+}
+
+
+/***
+ * create & get && delete Novel Comments
+ */
+export const createCommentNovel =async (req:Request, res:Response) => {
+    try {
+        const { userId, commentText, novelId} = req.body;
+        if (!userId || !commentText || !novelId) {
             return res.status(400).json({msg: "Contenu obligatoire"})
         }
         const NEW_COMMENT = await Comments.create({
-            userId:authId,
+            userId,
             commentText,
             novelId
         }) 
-        return res.status(201).json({msg:"commentaire créé", NEW_COMMENT})
+        res.status(201).json({msg:"commentaire créé", NEW_COMMENT})
     }
     catch (err) {
-        return res.status(400).json({msg:"Erreur creation de commentaire", err})
+        res.status(400).json({msg:"Erreur creation de commentaire", err})
     }
 }
 
-export const getAllNovelComment =async (res:Response, req:Request) => {
-    const novelId = req.params.novelId;
+export const getAllNovelComment =async (req:Request, res:Response) => {
+    const novelId = req.params.id;
     try {
         const comments = await Comments.findAll({where:{novelId:novelId}})
         if(!comments) {
             return res.status(400).json({msg:"Aucun commentaires retrouvées"})
         }
-        return res.status(200).json({comments})
+         res.status(200).json({comments})
     } catch (error) {
-        return res.status(400).json({msg: "Erreur de recuperation des commentaires des recettes", error})
+         res.status(400).json({msg: "Erreur de recuperation des commentaires des recettes", error})
     }
 }
 
-export const createCommentCocktail=async (res:Response, req:Request) => {
-    const cocktailId = req.params.cocktailId;
-    const authId = req.auth;
+export const deleteNovelComment =async (req:Request, res:Response) => {
+    const ID = req.params.id;
+    try {
+        const commentToDelected = await Novel.destroy({where:{commentId:ID}});
+        if(!commentToDelected) {
+            return res.status(400).json({msg: "Commentaire introuvable"})
+        }
+        return res.status(200).json({msg: "Commentaire supprimé avec succes"})
+    } catch (error) {
+        return res.status(400).json({msg: "Erreur suppression du commentaire"})
+    }
+}
+
+/***
+ * create & get && delete cocktail Comments
+ */
+export const createCommentCocktail=async (req:Request, res:Response) => {
+    const cocktailId = req.params.id;
     try {
         const { userId, commentText } = req.body;
         if (!userId || !commentText) {
             return res.status(400).json({msg: "Contenu obligatoire"})
         }
         const NEW_COMMENT = await Comments.create({
-            userId:authId,
+            userId,
             commentText,
-            cocktailId
+            cocktailId 
         }) 
-        return res.status(201).json({msg:"commentaire créé", NEW_COMMENT})
+         res.status(201).json({msg:"commentaire créé", NEW_COMMENT})
     }
     catch (err) {
-        return res.status(400).json({msg:"Erreur creation de commentaire", err})
+         res.status(400).json({msg:"Erreur creation de commentaire", err})
     }
 }
 
-export const getAllCocktailComment =async (res:Response, req:Request) => {
-    const cocktailId = req.params.cocktailId;
+export const getAllCocktailComment =async ( req:Request, res:Response) => {
+    const cocktailId = req.params.id;
     try {
         const comments = await Comments.findAll({where:{cocktailId:cocktailId}})
         if(!comments) {
             return res.status(400).json({msg:"Aucun commentaires retrouvées"})
         }
-        return res.status(200).json({comments})
+         res.status(200).json({comments})
     } catch (error) {
-        return res.status(400).json({msg: "Erreur de recuperation des commentaires des recettes", error})
+         res.status(400).json({msg: "Erreur de recuperation des commentaires des recettes", error})
+    }
+}
+
+export const deleteCocktailComment =async (req:Request, res:Response) => {
+    const ID = req.params.id;
+    try {
+        const commentToDelected = await Cocktail.destroy({where:{commentId:ID}});
+        if(!commentToDelected) {
+            return res.status(400).json({msg: "Commentaire introuvable"})
+        }
+        return res.status(200).json({msg: "Commentaire supprimé avec succes"})
+    } catch (error) {
+        return res.status(400).json({msg: "Erreur suppression du commentaire"})
     }
 }
