f451f51ca7ed5d2ccfe99979f04ec7271e56ca87
Koro0 <ledane92@gmail.com>
2023-04-24 16:10:25 +0200

    add delete comment route & adminAuth

--- src/routes/comments.route.ts
+++ src/routes/comments.route.ts
@@ -1,25 +1,34 @@
 import {Router, RequestHandler}  from 'express';
 import authenticate from '../middlewares/auth';
-
+import authenticateAdmin from '../middlewares/authAdmin';
 import {
     createCommentRecipe,
     getAllRecipeComment,
+    deleteCommentRecipe,
     createCommentNovel,
     getAllNovelComment,
+    deleteNovelComment,
     createCommentCocktail,
     getAllCocktailComment,
+    deleteCocktailComment,
 } from '../controllers/comments.controllers';
 const router: Router = Router();
 const auth:RequestHandler = authenticate;
+const adminAuth:RequestHandler = authenticateAdmin;
 
 /**
  * Route for Comments
  */
 router.post('/recipe', auth, createCommentRecipe);
 router.get('/recipe/:id', getAllRecipeComment);
+router.delete('/recipe/:id', adminAuth, deleteCommentRecipe);
+
 router.post('/novel', auth, createCommentNovel);
 router.get('/novel/:id', getAllNovelComment);
+router.get('/novel/:id', adminAuth, deleteNovelComment);
+
 router.post('/cocktail', auth, createCommentCocktail);
 router.get('/cocktail/:id', getAllCocktailComment);
+router.get('/cocktail/:id', adminAuth, deleteCocktailComment);
 
 export default router;
