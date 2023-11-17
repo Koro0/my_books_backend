4b6d129d592f43b6d0b3a877bcde538f6c2e9d0d
Koro0 <ledane92@gmail.com>
2023-11-17 15:25:25 +0100

    create prod route

---
+++ src/routes/product.route.ts
@@ -0,0 +1,12 @@
+import { Router, RequestHandler } from 'express';
+import authenticate from '../middlewares/authAdmin';
+
+import { createProduct, getAllProd } from '../controllers/product/product.ctrl';
+
+const multer = require('../middlewares/multer-config');
+const router: Router = Router();
+const auth: RequestHandler = authenticate;
+
+router.post('/', multer, auth, createProduct);
+
+export default router;
