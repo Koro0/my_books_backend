4fe056a57deffba13018d72430afd4d983830ead
Koro0 <ledane92@gmail.com>
2023-11-27 14:12:24 +0100

    create image model

---
+++ src/models/image.model.ts
@@ -0,0 +1,28 @@
+import { Table, Column, Model, DataType } from 'sequelize-typescript';
+
+@Table({
+  tableName: 'Image',
+})
+export class Image extends Model {
+  @Column({
+    type: DataType.INTEGER.UNSIGNED,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  imageId!: number;
+
+  @Column({
+    type: DataType.STRING,
+  })
+  type?: string;
+
+  @Column({
+    type: DataType.STRING,
+  })
+  name?: string;
+
+  @Column({
+    type: DataType.BLOB,
+  })
+  data?: any;
+}
